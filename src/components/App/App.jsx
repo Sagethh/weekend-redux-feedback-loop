12306d769a5ae297e41a086e3ffa8ec4b6437f28
Sagethh <tate.feige@gmail.com>
2021-07-17 13:28:17 -0500

    added some default stuff to app.jsx including all the routes (I think) that will be used as well as a function for getting feedback data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Comments from '../Comments/Comments';
+import Feeling from '../Feeling/Feeling';
+import Home from '../Home/Home';
+import Support from '../Support/Support';
+import Understanding from '../Understanding/Understanding';
+import Review from '../Review/Review';
+import Success from '../Success/Success';
 
-function App() {
+function App() { // main function for this file
 
-  return (
+  const fetchFeedback = () => { // main function to get feedback from data
+    axios({ // axios GET request
+      method: 'GET',
+      url: '/api/feedback'
+    })
+    .then(response => { // after getting data, set the payload to the response given
+      dispatchEvent({
+        type: 'SET_FEEDBACK',
+        payload: response.data
+      });
+    })
+    .catch(error => { // catch any errors
+      console.log(error);
+    });
+  };
+ 
+  return ( // what to show up on DOM, also includes the routing that will be needed
+    <Router>                                   
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+        <Route path="/" exact component={Home} />
+        <Route path="/feeling"  component={Feeling} /> 
+        <Route path="/understanding" component={Understanding} />
+        <Route path="/support" component={Support} /> 
+        <Route path="/comments" component={Comments} />
+        <Route path="/review"  component={Review} />
+        <Route path="/success" component={Success} />
     </div>
+    </Router>
   );
-}
+};
 
 export default App;
