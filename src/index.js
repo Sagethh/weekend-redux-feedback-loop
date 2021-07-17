7b8b5e5ee600a6ad9676f45460cb966b9ea2ac37
Sagethh <tate.feige@gmail.com>
2021-07-17 13:40:46 -0500

    added some stuff to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
-  
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feedbackReducer = (state = [], action) => {
+
+  switch(action.type) {
+      case 'SET_FEELING':
+          return{...state, feeling: action.payload};
+
+      case 'SET_UNDERSTANDING':
+          return{...state, understanding: action.payload};
+
+      case 'SET_SUPPORT':
+          return{...state, support: action.payload};
+
+      case 'SET_COMMENTS':
+          return{...state, comments: action.payload};
+
+      default:
+          return state;
+  }
+};
+
+const storeInstance = createStore(
+  combineReducers({
+    feedbackReducer
+  }),
+  applyMiddleware(logger)
+);
+
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
